b31cb5fe91f9ae1639539fa830306cb2d8c12163
Koichiro Taoka <koichirotaoka@gmail.com>
2018-11-06 08:39:57 +0000

    PictureMailer.picture_mail(@picture).deliver消去

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -23,7 +23,6 @@ class PicturesController < ApplicationController
     @picture = Picture.new(picture_params)
     @picture.user_id = current_user.id 
     if @picture.save
-      PictureMailer.picture_mail(@picture).deliver  ##追記
       redirect_to pictures_path, notice: 'Complete！'
     else
       render 'new'
